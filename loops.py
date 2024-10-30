1e0d0736dd1df0fcaf2c5236e694142b67d62fe4
Salazar <asalazar58@cps.k12.il.us>
2024-10-30 11:56:54 -0500

    LOOPS and While

---
+++ loops.py
@@ -0,0 +1,52 @@
+#while loop = execute some code WHILE spome condition remains true
+# name = input("enter your name: ")
+# if name=="":
+#     print("You did not enter your name")
+# else:
+#     print(f"hello {name}")
+##########################
+# name = input("enter your name: ")
+#while will keep repeating this code over and over until name does not equal to a blank space
+# while name=="":
+#     print("You did not enter your name: ")
+#     name=input("enter your name") #if you remove this line, the code will run infinetly and will cause the computer to crash
+# else:
+#     print(f"hello {name}")
+
+# age= int(input("Enter your name: "))
+# while age < 0:
+#     print("Age can't be negative")
+#     age=int(input("Enter your age: "))
+# print(f"You are {age} years old")
+###########################
+# food= input("Enter a food you like (q to quit): ")
+
+# while not food == "q":
+#     print(f"you like {food}")
+#     food= input("Enter another food you like (q to quit): ")
+#     print("Bye")
+##
+# num= int(input("Enter a number 1-10: "))
+# while num < 1 or num > 10:
+#     print(f"{num} is not valid")
+#     num = int(input("Enter a number between 1-10: "))
+#     print(f"Your number is {num}")
+
+########NOTES FOR LOOPS##########################
+#for loops = execute a block of code a fixed number of times you can iterate over a reange, string, sequenece. 
+
+# for x in (range(1,11,2)): # reversed(range(1,11)) will count backwords (x,x,x) the third number will make it count in twos
+#     print(x)
+
+# credit_card= "1234-5678-9012-3456"
+# for x in credit_card:
+#     print(x)
+#######################################
+#iterate means LOOP
+
+# for x in range(1,21):
+#     if x==13:
+#         break  #contiune basically skip that varaible # Break stops the code at that number
+#     else:
+#         print(x)
+
