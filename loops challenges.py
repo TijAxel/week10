a15a780d12204dd16541034c4e54f99edc545ffc
Salazar <asalazar58@cps.k12.il.us>
2024-10-30 12:02:04 -0500

    Loops and WHile

---
+++ loops challenges.py
@@ -0,0 +1,7 @@
+#challenge
+list_of_names={'John', 'paul', 'George', 'Ringo'}
+#If the name is 'George' , print 'Geore was found:'
+#otherwise, print "George wa not found" and print out the other names using a loop
+#Loop through the list_of_names2
+list_of_names2=['Thanos', 'Ironman', 'Thor', 'Hulk']
+
