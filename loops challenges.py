b7c7d4fda220214972f2ec1f38fb80a3b8ebf333
Salazar <asalazar58@cps.k12.il.us>
2024-10-30 12:15:14 -0500

    Idek what the gell happen here

--- loops challenges.py
+++ loops challenges.py
@@ -5,3 +5,19 @@ list_of_names={'John', 'paul', 'George', 'Ringo'}
 #Loop through the list_of_names2
 list_of_names2=['Thanos', 'Ironman', 'Thor', 'Hulk']
 
+for name in list_of_names:
+    if name == 'George':
+        print("George has been found!!")
+else:
+    print('George was not found :(')
+    print(name)
+for name in list_of_names2:
+    if name =='Thanos':
+        name=='Black Widow'
+    print(name)
+
+list_of_names2 = ['Thanos', 'Ironman', 'Thor', 'Hulk']
+for i in range(len(list_of_names2)):
+    if list_of_names2[i]== 'Thanos':
+        list_of_names2[i]='Black Widow'
+        print(list_of_names2[i])
